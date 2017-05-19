8c6f7ea2b4a05ca1a1dd3c7ce56af7834aa15115
timrooke1991 <timrooke1991@gmail.com>
2017-05-19 16:48:09 +0100

    Timing running, play/pause, random number generation

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,58 @@
-console.log('JS Loaded');
+$(() => {
+  const $matchTime = $('.changing-minutes');
+  const $commentaryBox = $('#commentaryBox');
+  const $primaryButton = $('.primary-button');
+  let $homeScore = $('.home-team .score');
+  let $awayScore = $('.away-team .score');
+
+  let matchTime = 0;
+  let run = false;
+
+  // Event Listeners
+  $primaryButton.on('click', () => {
+    run = !run;
+    run ? $primaryButton.text('Pause') : $primaryButton.text('Play');
+  });
+
+  // Loop through each second
+  const timerID = setInterval(function() {
+    if (run) {
+      const eventValue = genRandomValue(100) + 1;
+      console.log(eventValue);
+      if (eventValue % 5 === 0) {
+        const getScore = $homeScore.text();
+        const score = parseFloat(getScore) + 1;
+        $homeScore.text(score);
+        $commentaryBox.text(`Goal!!!`);
+
+      }
+
+      timeControl();
+    }
+  },100);
+
+// Functions
+
+  function timeControl() {
+
+    if(matchTime === 44) {
+      $commentaryBox.text(`That's half-time!`);
+      run = !run;
+      $primaryButton.text('Play');
+    }
+
+    if(matchTime === 90) {
+      clearInterval(timerID);
+      $primaryButton.text('Finish');
+    } else {
+      matchTime++;
+      $matchTime.text(matchTime);
+    }
+
+  }
+
+  function genRandomValue(value) {
+    return (Math.floor(Math.random() * value));
+  }
+
+});
---
+++ src/js/data.js
@@ -0,0 +1,45 @@
+const spurs = {
+  id: 0,
+  name: 'spurs',
+  attack: 10000,
+  defence: 10000,
+  discipline: 10000,
+  players: [{
+    name: 'Kane',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    eligible: false
+  },{
+    name: 'DeleAlli',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    eligible: false
+  },{
+    name: 'Eriksen',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    eligible: false
+  },{
+    name: 'Dembele',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    eligible: false
+  },{
+    name: 'Son',
+    attack: 50,
+    defence: 50,
+    discipline: 1000,
+    status: '',
+    eligible: false
+  }]
+};
