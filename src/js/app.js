7155ea7b5ca7f8ed6a765afdbc3cbabcd1b4ad4e
timrooke1991 <timrooke1991@gmail.com>
2017-05-21 11:08:09 +0100

    team styling added

--- src/js/app.js
+++ src/js/app.js
@@ -4,48 +4,70 @@ $(() => {
   const $primaryButton = $('.primary-button');
   const $homeScore = $('.home-team .score');
   const $awayScore = $('.away-team .score');
+  // const $homeTeam = $('.home-team');
+  // const $awayTeam = $('.away-team');
 
   let matchTime = 0;
   let run = false;
+  let width = 50;
   let homeTeam = null;
   let awayTeam = null;
-  homeTeam = spurs;
-  awayTeam = arsenal;
 
 
-  // Loop through each second
+  // MATCH LOGIC
   const timerID = setInterval(function() {
     if (run) {
 
       const eventValue = genRandomValue(100) + 1;
 
-
       const [teamObject, teamString] = selectTeam();
       const chosenPlayer = selectPlayer(teamObject);
-
+      console.log(chosenPlayer);
       // Who has the event?
-
-
-      if (eventValue % 15 === 0) {
-        handleFreekick(chosenPlayer, teamString);
-      }
-      if (eventValue % 20 === 0) {
-        handleDiscipline(chosenPlayer, teamString);
+      if (eventValue % 2 === 0) {
+        goalChance(teamString, chosenPlayer);
       }
 
-      if (eventValue % 30 === 0) {
-        goalChance(teamObject,teamString, chosenPlayer);
-      }
-
-      if (eventValue % 75 === 0) {
-        handleInjury(chosenPlayer, teamString);
-      }
-
+      // if (eventValue % 80 === 0) {
+      //   handlePenalty(chosenPlayer, teamString);
+      // }
+      //
+      // if (eventValue % 95 === 0) {
+      //   straightRed(chosenPlayer, teamString);
+      // }
+      //
+      // if (eventValue % 12 === 0) {
+      //   handleFreekick(chosenPlayer, teamString);
+      // }
+      // if (eventValue % 20 === 0) {
+      //   handleDiscipline(chosenPlayer, teamString);
+      // }
+      //
+      // if (eventValue % 75 === 0) {
+      //   handleInjury(chosenPlayer, teamString);
+      // }
+      move();
       timeControl();
     }
-  },100);
+  },200);
 
   // Event Listeners -------------------------------------------------------------
+
+  $('.team-input').on('click', (e) => {
+    const userTeam = e.target.innerHTML;
+    console.log(e);
+    if (homeTeam === null) {
+      configTeam(homeTeam, userTeam);
+      console.log('homeTeam now selected!');
+      $('.title-message').text('Home team chosen. Now chose your opponent.');
+      // e.addClass('selected');
+    } else {
+      configTeam(awayTeam, userTeam);
+      console.log('awayTeam now selected!');
+      $('#match-setup, .info-message').hide();
+      $('#match-engine, .away-team, .home-team, .timer').show();
+    }
+  });
 
   $primaryButton.on('click', () => {
     run = !run;
@@ -76,13 +98,23 @@ $(() => {
     return (Math.floor(Math.random() * value));
   }
 
+  function move() {
+    const elem = document.getElementById('homeBar');
+    width = homeTeam.averagePlayerValues('attack') + genRandomValue(10);
+    elem.style.width = width + '%';
+  }
+
   function selectTeam() {
     const homeRandom =  genRandomValue(homeTeam.averagePlayerValues('attack'));
     const awayRandom =  genRandomValue(awayTeam.averagePlayerValues('attack'));
 
     if (homeRandom >= awayRandom) {
+      $commentaryBox.css('background-color', homeTeam.colors[0]);
+      $commentaryBox.css('color', homeTeam.colors[1]);
       return [homeTeam, 'home'];
     } else {
+      $commentaryBox.css('background-color', awayTeam.colors[0]);
+      $commentaryBox.css('color', awayTeam.colors[1]);
       return [awayTeam, 'away'];
     }
   }
@@ -92,46 +124,47 @@ $(() => {
     return team.players[genRandomIndex];
   }
 
-  function goalChance(object, team, player) {
+  function goalChance(team, player) {
+    if (player.playing) {
+      // Refactor this
+      let score = null;
+      let getScore = null;
+      if (team === 'home') {
+        getScore = $homeScore.text();
+        score = parseFloat(getScore) + 1;
+        $homeScore.text(score);
+      } else {
+        getScore = $awayScore.text();
+        score = parseFloat(getScore) + 1;
+        $awayScore.text(score);
+      }
+      $commentaryBox.text(`Goal!!!`);
 
-    // Refactor this
-    let score = null;
-    let getScore = null;
-    if (team === 'home') {
-      getScore = $homeScore.text();
-      score = parseFloat(getScore) + 1;
-      $homeScore.text(score);
-    } else {
-      getScore = $awayScore.text();
-      score = parseFloat(getScore) + 1;
-      $awayScore.text(score);
+      $(`#${team}Events`).append(`<i class='fa fa-futbol-o' style='font-size: 22px; color:white; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} scored<br/>`);
     }
-
-    $commentaryBox.text(`Goal!!!`);
-
-    $(`#${team}Events`).append(`<i class='fa fa-futbol-o' style='font-size: 22px; color:white; padding-top:5px' aria-hidden='true'></i>${matchTime} mins: ${player.name} scored<br/>`);
-
-
   }
 
   function handleDiscipline(player, team) {
-    if (player.status !== 'Ejected') {
+    if (player.status) {
       // Already booked? Send him off!
       console.log(player.status);
       if (player.status === 'Yellow') {
-
+        $commentaryBox.css('background-color', '#FF0000');
+        $commentaryBox.css('color', '#FFFFFF');
         // Store mesages and details in an game Object
         $commentaryBox.text(`${player.name} gets a red!`);
-        $(`#${team}Events`).append(`<i class='fa fa-square event-item' style='font-size: 24px; color: red; padding-top:5px' aria-hidden='true'></i>${matchTime} mins: ${player.name} sent off<br/>`);
+        $(`#${team}Events`).append(`<i class='fa fa-square event-item' style='font-size: 24px; color: red; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} sent off<br/>`);
 
         player.status = 'Ejected';
         player.playing = false;
 
       } else {
+        $commentaryBox.css('background-color', 'yellow');
+        $commentaryBox.css('color', '#000000');
         $commentaryBox.text(`${player.name} gets a yellow!`);
 
         // Store mesages and details in an game Object
-        $(`#${team}Events`).append(`<i class='fa fa-square event-item' style='font-size: 24px; color: yellow; padding-top:5px' aria-hidden='true'></i>${matchTime} mins: ${player.name} booked<br/>`);
+        $(`#${team}Events`).append(`<i class='fa fa-square event-item' style='font-size: 24px; color: yellow; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} booked<br/>`);
 
         player.status = 'Yellow';
       }
@@ -144,7 +177,7 @@ $(() => {
       // THERE COULD BE AN INJURY
       if (genRandomValue(101) % 8 === 0) {
         $commentaryBox.text(`${player.name} gets taken off on a stretcher!`);
-        $(`#${team}Events`).append(`<i class='fa fa-plus' style='font-size: 26px; color: green; padding-top:5px' aria-hidden='true'></i>${matchTime} mins: ${player.name} injured<br/>`);
+        $(`#${team}Events`).append(`<i class='fa fa-plus' style='font-size: 26px; color: green; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} injured<br/>`);
         player.playing = false;
       } else {
         $commentaryBox.text(`${player.name}'s limping, but he'll be okay`);
@@ -180,9 +213,147 @@ $(() => {
     }
   }
 
+  function handlePenalty(player, team) {
+    if (player.status) {
+      if (genRandomValue(101) % 5 === 0) {
+        // Refactor this
+        let score = null;
+        let getScore = null;
+        if (team === 'home') {
+          getScore = $homeScore.text();
+          score = parseFloat(getScore) + 1;
+          $homeScore.text(score);
+        } else {
+          getScore = $awayScore.text();
+          score = parseFloat(getScore) + 1;
+          $awayScore.text(score);
+        }
+
+        $commentaryBox.text(`${player.name}'s clinical from the spot! Goal!!!`);
+
+        $(`#${team}Events`).append(`<i class='fa fa-futbol-o' style='font-size: 22px; color:white; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} scores penalty<br/>`);
+
+      } else {
+        $commentaryBox.text(`He's missed it! The pressure has got to him`);
+        $(`#${team}Events`).append(`<i class='fa fa-futbol-o' style='font-size: 22px; color:red; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} scores penalty<br/>`);
+
+      }
+    }
+  }
+
+  function straightRed(player, team) {
+    if (player.playing) {
+      player.playing = false;
+      $commentaryBox.text('The referee gives him a straight red!');
+      $(`#${team}Events`).append(`<i class='fa fa-square' style='font-size: 24px; color: red; padding-top:5px' aria-hidden='true'></i> ${matchTime} mins: ${player.name} sent off<br/>`);
+    }
+  }
+
+
+
+
   function generateEventMessage() {
     // Store mesages and details in an game Object
 
+  }
+
+  function configTeam(team, selector) {
+    if (homeTeam === null) {
+      $('.home-team  .team-name').text(selector);
+      switch (selector) {
+        case 'Tottenham':
+          homeTeam = spurs;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Man United':
+          homeTeam = manUnited;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Liverpool':
+          homeTeam = liverpool;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Man City':
+          homeTeam = manCity;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Everton':
+          homeTeam = everton;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Chelsea':
+          homeTeam = chelsea;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Leicester':
+          homeTeam = leicester;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+        case 'Arsenal':
+          homeTeam = arsenal;
+          $('.home-team, #homeBar').css('background-color', homeTeam.colors[0]);
+          $('.home-team').css('color', homeTeam.colors[1]);
+          break;
+      }
+      console.log(homeTeam);
+    } else {
+
+      $('.away-team .team-name').text(selector);
+
+      switch (selector) {
+        case 'Tottenham':
+          awayTeam = spurs;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Man United':
+          awayTeam = manUnited;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Liverpool':
+          awayTeam = liverpool;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Man City':
+          awayTeam = manCity;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Everton':
+          awayTeam = everton;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Chelsea':
+          awayTeam = chelsea;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Leicester':
+          awayTeam = leicester;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+        case 'Arsenal':
+          awayTeam = arsenal;
+          $('.away-team, .possession-bar').css('background-color', awayTeam.colors[0]);
+          $('.away-team').css('color', awayTeam.colors[1]);
+          break;
+      }
+      console.log(awayTeam);
+    }
+
+    console.log(selector);
+    // Pass in the object values to homeTeam
   }
 
 });
--- src/js/data.js
+++ src/js/data.js
@@ -1,6 +1,7 @@
 const spurs = {
   id: 0,
   name: 'spurs',
+  colors: ['#FFFFFF', '#122B57'],
   formation: '4-4-2',
   players: [{
     name: 'Kane',
@@ -63,6 +64,7 @@ const spurs = {
 const arsenal = {
   id: 1,
   name: 'arsenal',
+  colors: ['#D90104','#FFFFFF'],
   formation: '4-5-1',
   players: [{
     name: 'Giroud',
@@ -102,6 +104,384 @@ const arsenal = {
     playing: true
   },{
     name: 'Walcott',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const manCity = {
+  id: 2,
+  name: 'manchester-city',
+  colors: ['#98C5E9', '#00285E'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Sane',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Aguero',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Yaya Toure',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Silva',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'De Bruyne',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const manUnited = {
+  id: 3,
+  name: 'manchester-united',
+  colors: ['#DD1E2A', '#FFFFFF'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Rooney',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Ibrahimovic',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Herrera',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Rashford',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'Young',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const everton = {
+  id: 4,
+  name: 'everton',
+  colors: ['#0A2C78', '#FFFFFF'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Lukaku',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Barkley',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Mirallas',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Baines',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'Coleman',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const leicester = {
+  id: 5,
+  name: 'leicester',
+  colors: ['#273E8A','#FFFFFF'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Vardy',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Mahrez',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Drinkwater',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Ulloa',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'Morgan',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const liverpool = {
+  id: 7,
+  name: 'liverpool',
+  colors: ['#E31B23', '#FFFFFF'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Sturridge',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Lallana',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Firmino',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Mane',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'Henderson',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 10,
+    status: '',
+    playing: true
+  }],
+  averagePlayerValues(string) {
+    const totalValues = this.players.reduce(function(total, value){
+      var sum = total + value[string];
+      return sum;
+    }, 0);
+    // this[string] = totalValues / 5;
+    return (totalValues / 5);
+    // Update to eleven
+    // Hard coded eleven so that when players are sent off team values suffer
+  }
+};
+
+const chelsea = {
+  id: 7,
+  name: 'chelsea',
+  colors: ['#304189', '#FFFFFF'],
+  formation: '4-5-1',
+  players: [{
+    name: 'Costa',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Willian',
+    age: 26,
+    position: 'Striker',
+    attack: 100,
+    defence: 50,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Matic',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 80,
+    status: '',
+    playing: true
+  },{
+    name: 'Hazard',
+    age: 26,
+    position: 'Striker',
+    attack: 50,
+    defence: 0,
+    discipline: 0,
+    status: '',
+    playing: true
+  },{
+    name: 'Moses',
     age: 26,
     position: 'Striker',
     attack: 50,
